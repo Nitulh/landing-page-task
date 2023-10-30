03fcfd5a602343df516c22229eec9a0c78c280e1
Nitul Arif <nitularif100@gmail.com>
2023-10-31 00:21:26 +0600

    about section

--- src/components/empty.jsx
+++ src/components/empty.jsx
@@ -1,31 +1,56 @@
 import { useEffect, useState } from "react";
 import "../styles/empty.css";
+import { GiChart, GiLightBulb, GiCalendar } from 'react-icons/gi';
+import One from "/images/1.avif";
+import Two from "/images/2.avif";
+import Three from "/images/3.avif";
+
 
 const Empty = () => {
-  const [scrollRange, setScrollRange] = useState(0);
+  const [scrollRanges, setScrollRanges] = useState({
+    mission: 0,
+    plan: 0,
+    vision: 0,
+  });
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
 
-      // Define your desired scroll range values (adjust as needed)
-      const startRange = 1800;
-      const endRange = 2000;
+      // Define your desired scroll range values for each section (adjust as needed)
+      // const startRangeMission = 1800;
+      // const endRangeMission = 2000;
+      const startRangeMission = 1400;
+      const endRangeMission = 1600;
+      const startRangePlan = 1600;
+      const endRangePlan = 1800;
+      const startRangeVision = 1800;
+      const endRangeVision = 2000;
 
-      if (scrollPosition >= startRange && scrollPosition <= endRange) {
-        const rangePercentage = (scrollPosition - startRange) / (endRange - startRange);
-        setScrollRange(rangePercentage);
-      } else if (scrollPosition < startRange) {
-        setScrollRange(0);
-      } else if (scrollPosition > endRange) {
-        setScrollRange(1);
-      }
+      const calculateScrollRange = (startRange, endRange) => {
+        if (scrollPosition >= startRange && scrollPosition <= endRange) {
+          return (scrollPosition - startRange) / (endRange - startRange);
+        }
+        if (scrollPosition < startRange) {
+          return 0;
+        }
+        if (scrollPosition > endRange) {
+          return 1;
+        }
+        return 0;
+      };
+
+      setScrollRanges({
+        mission: calculateScrollRange(startRangeMission, endRangeMission),
+        plan: calculateScrollRange(startRangePlan, endRangePlan),
+        vision: calculateScrollRange(startRangeVision, endRangeVision),
+      });
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
   return (
@@ -38,11 +63,11 @@ const Empty = () => {
         </p>
       </header>
       <div className="row about-cols">
-        <div className="col-md-4 about-col" style={{ opacity: scrollRange }}>
+        <div className="col-md-4 about-col" style={{ opacity: scrollRanges.mission }}>
           <div className="img">
-            <img src="assets/img/about-mission.jpg" alt="" className="img-fluid" />
+            <img src={One} alt="first_image" className="img-fluid" />
             <div className="icon">
-              <i className="bi bi-bar-chart"></i>
+              <GiChart />
             </div>
           </div>
           <h2 className="title">
@@ -52,11 +77,11 @@ const Empty = () => {
             Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </div>
-        <div className="col-md-4 about-col" style={{ opacity: scrollRange }}>
+        <div className="col-md-4 about-col" style={{ opacity: scrollRanges.plan }}>
           <div className="img">
-            <img src="assets/img/about-plan.jpg" alt="" className="img-fluid" />
+            <img src={Two} alt="second_image" className="img-fluid" />
             <div className="icon">
-              <i className="bi bi-brightness-high"></i>
+            <GiLightBulb/>
             </div>
           </div>
           <h2 className="title">
@@ -66,11 +91,11 @@ const Empty = () => {
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
           </p>
         </div>
-        <div className="col-md-4 about-col" style={{ opacity: scrollRange }}>
+        <div className="col-md-4 about-col" style={{ opacity: scrollRanges.vision }}>
           <div className="img">
-            <img src="assets/img/about-vision.jpg" alt="" className="img-fluid" />
+            <img src={Three} alt="third_image" className="img-fluid" />
             <div className="icon">
-              <i className="bi bi-calendar4-week"></i>
+             <GiCalendar/>
             </div>
           </div>
           <h2 className="title">
